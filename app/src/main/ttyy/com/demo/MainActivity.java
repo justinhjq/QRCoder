6a54b9179a44574072154f2fefd4d81c13987e32
yztazjin <hudadada0305@gmail.com>
2017-03-09 10:26:03 +0800

    Create MainActivity.java

---
+++ app/src/main/ttyy/com/demo/MainActivity.java
@@ -0,0 +1,61 @@
+package ttyy.com.demo;
+
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Window;
+
+import ttyy.com.coder.scanner.DecodeScannerView;
+import ttyy.com.coder.scanner.ScannerParentView;
+
+public class MainActivity extends AppCompatActivity implements ScannerParentView.DecodeStatusListener{
+
+    ScannerParentView qrcodeview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        qrcodeview = (DecodeScannerView) findViewById(R.id.qrcodeview);
+        qrcodeview.onCreate();
+
+        qrcodeview.setDecodeStatusListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        qrcodeview.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        qrcodeview.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        qrcodeview.onDestroy();
+    }
+
+    private void vibrate() {
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(200);
+    }
+
+    @Override
+    public void onDecodeSuccess(String data) {
+        Log.e("Test", "data "+data);
+        vibrate();
+    }
+
+    @Override
+    public void onDecodeError(Exception e) {
+        Log.d("Test", "e "+e.getMessage());
+    }
+}
